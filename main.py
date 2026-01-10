29e1231434c41eb3f5cdc270d241492e734005fa
unknown <nygaard.casper@gmail.com>
2026-01-10 19:44:52 +0100

    add postgreSQL integration

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from database import engine, Base
 
 app = FastAPI()
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def health():
